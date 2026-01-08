35f3aa8ce06ab6f57f0065a4f5bad45e39f70bac
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-08 10:19:44 +0530

    assignment operator

---
+++ operator/assignmentoperator.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class assignmentoperator {
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+        System.out.println("a+=b:"+(a+=b));
+        System.out.println("a-=b"+(a-=b));
+        System.out.println("a*=b:"+(a*=b));
+
+        System.out.println("a/=b:"+(a/=b));
+        System.out.println("a%=b:"+(a%=b));
+
+    }
+    
+}

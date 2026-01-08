03961c2d208b9045fc4a33ef1cd66012f480628a
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-08 10:46:48 +0530

    .

--- logicaloperator.java
+++ logicaloperator.java
@@ -2,12 +2,12 @@ public class logicaloperator {
     public static void main(String args[]){
         int a=10;
         int b=20;
+        int c=30;
         System.out.println("a&&b:"+(a>b&&b>a));
         System.out.println("a||b:"+(a>b||b>a));
         System.out.println("a!b"+!(a>b));
-
-
-
+        System.out.println("the greatest among three numbers is:"+(a>b && a>c));
+    
 
     }
     

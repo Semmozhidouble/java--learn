446b3e284b877ade622343b2bde01065d4985ecc
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-09 09:25:16 +0530

    logicaloperator

--- logicaloperator.java
+++ logicaloperator.java
@@ -3,10 +3,15 @@ public class logicaloperator {
         int a=10;
         int b=20;
         int c=30;
+        //find the greatest number among three numbers using logical operator
         System.out.println("a&&b:"+(a>b&&b>a));
         System.out.println("a||b:"+(a>b||b>a));
         System.out.println("a!b"+!(a>b));
         System.out.println("the greatest among three numbers is:"+(a>b && a>c));
+        System.out.println("the greatest among three numbers is:"+(b>a && b>c));
+        System.out.println("the greatest among three numbers is:"+(c>a && c>b));
+        //write a program to ch
+        
     
 
     }

cb907cc785cb728fd2d716d65ea2670df7009d93
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-09 09:55:12 +0530

    ternary operator

---
+++ operator/ternary.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class ternary {
+    public static void main(String args[]){
+        int age=19;
+        System.out.println((age>18)?"yes you are eligible to vote":"no you are not eligible");
+
+    } 
+    
+}
--- operator/unaryoperator.java
+++ operator/unaryoperator.java
@@ -5,10 +5,10 @@ public class unaryoperator {
         int a=10;
         System.out.println("a:"+a);
         System.out.println("unary plus:"+(+a));
-        System.out.println("unary plus:"+(-a));
+        System.out.println("unary minus:"+(-a));
         System.out.println("pre increment:"+(++a));
         System.out.println("post increment:"+(a++));
-        System.out.println("pre decrement:"+(--a));
+        System.out.println("pre decrement:"+(a--));
 
 
 

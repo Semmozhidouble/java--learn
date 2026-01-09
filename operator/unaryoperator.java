9bed0884f7240f0d5c564333445421509df1eba6
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-09 09:40:23 +0530

    unary operator

---
+++ operator/unaryoperator.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class unaryoperator {
+    public static void main(String args[]){
+        int a=10;
+        System.out.println("a:"+a);
+        System.out.println("unary plus:"+(+a));
+        System.out.println("unary plus:"+(-a));
+        System.out.println("pre increment:"+(++a));
+        System.out.println("post increment:"+(a++));
+        System.out.println("pre decrement:"+(--a));
+
+
+
+    }
+    
+}

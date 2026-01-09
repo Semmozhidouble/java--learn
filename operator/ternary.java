205199a4a2273ae5fdb65504d0734091e36d79ed
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-09 10:03:07 +0530

    .

--- operator/ternary.java
+++ operator/ternary.java
@@ -2,7 +2,7 @@ package operator;
 
 public class ternary {
     public static void main(String args[]){
-        int age=19;
+        int age=17;
         System.out.println((age>18)?"yes you are eligible to vote":"no you are not eligible");
 
     } 

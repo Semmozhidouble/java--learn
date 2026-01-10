f7d2515554a51f360e91bd8ad91428f9678d064d
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-10 12:08:42 +0530

    methods

---
+++ methods.java
@@ -0,0 +1,14 @@
+public class methods {
+    public static void main(String[] args){
+        String name="is a good choice";
+        float price=99.99f; 
+        car(name,price);  
+        
+    }
+    
+
+static void car(String name,float price){
+    System.out.println("car "+name);
+    System.out.println("price of the car is :"+price);
+}
+}

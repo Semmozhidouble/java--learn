f3ff2e84a1fd5c215f1cc665b762ca0efbb4c395
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-15 20:02:53 +0530

    printing value 1 to 100

---
+++ sums/1to100.java
@@ -0,0 +1,12 @@
+package sums;
+
+public class 1to100 {
+    public static void main(String[] args){
+        int sum=0;
+        for(i=0;i<=100;i++){
+            sum=sum+i;
+            System.out.println("the sum is "+sum);
+        }
+    }
+    
+}

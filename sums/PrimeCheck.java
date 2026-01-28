84f41c395f9ab165f53d541f12ad7eaf1f04dd0c
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-28 10:30:01 +0530

    prime check

---
+++ sums/PrimeCheck.java
@@ -0,0 +1,25 @@
+package sums;
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        boolean isPrime=true;
+        for(int i=2;i<num;i++){
+            if(num%i==0){
+                isPrime=false;
+                break;
+            }
+            
+        }
+        if(isPrime){
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
+        }
+        sc.close();
+    }
+    
+}

aa01048c85967a633b2487775038e312fbc02a20
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-29 14:17:31 +0530

    .

--- sums/LeapYear.java
+++ sums/LeapYear.java
@@ -1,7 +1,7 @@
 package sums;
 import java.util.Scanner;
 
-public class LeapYear {
+public class LeapYear {2
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the year:");

833077fdb14ff148a36e442f8455acdb475dd9ba
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-29 13:48:04 +0530

    .

---
+++ sums/IntToString.java
@@ -0,0 +1,12 @@
+package sums;
+
+
+public class IntToString {
+    public static void main(String[] args){
+        int num=123;
+        String str=String.valueOf(num);
+        System.out.println("Integer:"+num);
+        System.out.println("Converted string:"+str);
+    }
+    
+}

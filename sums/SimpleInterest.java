83006fe474acd0d1c50863e19864c6c0de4a83ec
Semmozhidouble <semmozhi12122005@gmail.com>
2026-01-30 11:17:06 +0530

    count of digits

--- sums/SimpleInterest.java
+++ sums/SimpleInterest.java
@@ -1,25 +1,16 @@
 package sums;
-
 import java.util.Scanner;
-
 public class SimpleInterest {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter the principal amount: ");
         double p = sc.nextDouble();
-
         System.out.print("Enter the rate of interest: ");
         double r = sc.nextDouble();
-
         System.out.print("Enter the time (in years): ");
         double t = sc.nextDouble();
-
         double si = (p * r * t) / 100;
-
         System.out.println("Simple Interest: " + si);
-
         sc.close();
     }
 }

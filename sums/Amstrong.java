5df42447e683c85b8e814ce1017121ad4588ec02
Semmozhidouble <semmozhi12122005@gmail.com>
2026-02-02 09:43:37 +0530

    amstrong number changed

--- sums/Amstrong.java
+++ sums/Amstrong.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public  class Amstrong{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
         int num=sc.nextInt();
         int temp=num;
         int digits = String.valueOf(num).length();

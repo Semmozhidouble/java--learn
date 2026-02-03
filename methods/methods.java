1c36b0ec29728451b6d82bc4eb60ff3ff687c2de
Semmozhidouble <semmozhi12122005@gmail.com>
2026-02-03 09:31:56 +0530

    just modified the static methods by adding access modifier

--- methods/methods.java
+++ methods/methods.java
@@ -3,12 +3,12 @@ public class methods {
     public static void main(String[] args){
         String name="is a good choice";
         float price=99.99f; 
-        car(name,price);  
+        car(name,price);    
         
     }
     
 
-static void car(String name,float price){
+    public static void car(String name,float price){
     System.out.println("car "+name);
     System.out.println("price of the car is :"+price);
 }

2309029da0a573ec64410fa4ca7a8514c7df4b70
Semmozhidouble <semmozhi12122005@gmail.com>
2026-02-03 09:41:55 +0530

    .

---
+++ methods/Friends.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class Friends {
+    public static void main(String[] args){
+        String name="Is My Friend";
+        friend(name);
+    }
+    public static void friend(String name){
+        System.out.println("sarath"+name);
+    }
+    
+
+    
+}

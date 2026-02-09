8375f06ddc8bc2bcb1348c4b0b769562cf330799
Semmozhidouble <semmozhi12122005@gmail.com>
2026-02-09 14:09:49 +0530

    .

---
+++ sums/CharFromaTOb.java
@@ -0,0 +1,11 @@
+package sums;
+
+
+public class CharFromaTOb {
+    public static void main(String[] args){
+        for(int i='a';i<='z';i++){
+            System.out.println(i);
+        }
+    }
+    
+}

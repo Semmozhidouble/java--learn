80f4e9c66a1739a0e0992eb30adf578d62a2a66f
Semmozhidouble <semmozhi12122005@gmail.com>
2026-02-09 14:02:24 +0530

    char to ascii value

---
+++ sums/CharFromAToZ.java
@@ -0,0 +1,16 @@
+package sums;
+import java.util.Scanner;
+
+public class CharFromAToZ {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        for(int i='A';i<='Z';i++){
+            System.out.println(i);
+        }
+        
+        
+
+
+    }
+    
+}

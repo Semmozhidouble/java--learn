a20acfba9c88e58c5da28cdd26e2025783797a45
Semmozhidouble <semmozhi12122005@gmail.com>
2026-02-10 14:00:35 +0530

    .

--- sums/CharToString.java
+++ sums/CharToString.java
@@ -2,6 +2,15 @@ package sums;
 import java.util.Scanner;
 
 public class CharToString {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the char");
+        char c=sc.next().charAt(0);
+        String str=String.valueOf(c);
+        System.out.println(c);
+        System.out.println(str);
+        
+    }
     
     
 }

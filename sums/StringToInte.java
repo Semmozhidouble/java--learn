e6f0b31bb86244f75f066617dab367075fdcc78d
Semmozhidouble <semmozhi12122005@gmail.com>
2026-02-10 14:08:25 +0530

    .

---
+++ sums/StringToInte.java
@@ -0,0 +1,15 @@
+package sums;
+import java.util.Scanner;
+
+public class StringToInte {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str=sc.next();
+        int c=Integer.parseInt(str);
+        System.out.println(c);
+        sc.close();
+        
+    }
+    
+}

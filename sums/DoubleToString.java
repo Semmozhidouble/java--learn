5294b218a4f2b596260ace52f84637b88f834489
Semmozhidouble <semmozhi12122005@gmail.com>
2026-02-10 14:10:13 +0530

    . .#

---
+++ sums/DoubleToString.java
@@ -0,0 +1,16 @@
+package sums;
+import java.util.Scanner;
+
+public class DoubleToString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the double");
+        double d=sc.nextDouble();
+        String str=String.valueOf(d);
+        System.out.println(d);
+        System.out.println(str);
+        sc.close();
+        
+    }
+    
+}
